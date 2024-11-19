987a24152acce7085c69f1e213eb92e4d76bb515
Gabriel Augusto <Gabriel.afg182@gmail.com>
2024-11-19 10:31:32 -0300

    Aula 01 - Finalizada

--- src/main/java/com/biel/createUrlShortner/Main.java
+++ src/main/java/com/biel/createUrlShortner/Main.java
@@ -1,7 +1,37 @@
 package com.biel.createUrlShortner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = input.get("body").toString();
+
+        Map<String, String> bodyMap;
+        try {
+            bodyMap = objectMapper.readValue(body, Map.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error parsing body");
+        }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expiration = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+
+        return response;
     }
 }
